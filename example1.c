b242463ab17ed84b3fb5f3b10654a8c04e7f667d
Robert D. French <frenchrd@ornl.gov>
2014-03-24 10:32:19 -0400

    Example 1 Knows about Tony

--- example1.c
+++ example1.c
@@ -4,15 +4,15 @@
 #include "suspect.h"
 
 suspect_context("Euler's Method",
-	suspect_test("Predicts y_{n+1} to within the expected context",
-		suspect_assert("Expected 1 < 0", 1 < 0);
+	suspect_test("Predicts y_{n+1} to within the expected threshhold",
+		suspect_assert("Expected 1 < 0", -1 < 0);
 	)
 
 	suspect_test("Does not explode",
 		suspect_assert("Expected -1 < 0", -1 < 0);
 	)
 
-	suspect_test("Does, in fact, explode",
-		suspect_assert("Tony is a dork", -1 < 1);
+	suspect_test("Knows about Tony",
+		suspect_assert("Tony is a dork", 2 < 1);
 	)
 )
